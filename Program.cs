def8eafe1d6dadd1673a614c40a1c1fcadbee3ee
Leonardo Caballero <patoleo2013@gmail.com>
2024-06-24 22:45:47 -0300

    detalles

--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ string numIngresado = Console.ReadLine();
 if (Int32.TryParse(numIngresado, out int num) && num>0) {
     char[] cadena = numIngresado.ToCharArray();
     Array.Reverse(cadena);
+    Console.WriteLine("Número invertido:");
     Console.WriteLine(cadena);
 } else {
     Console.WriteLine("Número ingresado inválido");
 }
+
+
