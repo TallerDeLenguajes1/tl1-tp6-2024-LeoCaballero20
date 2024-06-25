26130fa55e5854d3a596cba6c2255bd35139af1b
Leonardo Caballero <patoleo2013@gmail.com>
2024-06-24 22:41:06 -0300

    primer ejercicio hecho

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese un número entero mayor a 0");
+string numIngresado = Console.ReadLine();
+if (Int32.TryParse(numIngresado, out int num) && num>0) {
+    char[] cadena = numIngresado.ToCharArray();
+    Array.Reverse(cadena);
+    Console.WriteLine(cadena);
+} else {
+    Console.WriteLine("Número ingresado inválido");
+}
